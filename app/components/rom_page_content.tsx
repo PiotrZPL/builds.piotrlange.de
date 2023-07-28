dde1d03d93baa4871c93f90eb286d46361e8949b
PiotrZPL <zpl20052020@protonmail.ch>
2023-07-28 15:44:58 +0200

    Built up builds section

--- app/components/rom_page_content.tsx
+++ app/components/rom_page_content.tsx
@@ -12,23 +12,51 @@ export default function RomPageContent( props: any ) {
     return (
         <main className="flex flex-col items-center justify-between pt-12 md:pt-24">
             <div className='flex flex-col md:flex-row w-full'>
-                <div className='w-0 md:w-1/4'></div>
-                <div className='md:w-1/4'>
-                    <div className='flex flex-col items-center'>
+                <div className='w-0 md:w-1/6'></div>
+                <div className='md:w-2/6 bg-gray-800 rounded-lg border-2 md:border-4 border-white'>
+                    <div className='flex flex-col'>
                         {/* <div className='w-1/12 md:w-0'></div> */}
-                        <div className='px-4 md:px-0'>
+                        <div className='px-4'>
                             <h1 className='font-bold text-4xl'>{props["romName"]}</h1>
-                            <div className='h-10'></div>
+                            <div className='h-2 md:h-4'></div>
                             <h2 className='font-bold text-xl'>Supported on {romDevices.length} device</h2>
                             <h2 className='font-bold text-xl'>Device name: {name}</h2>
                             <h2 className='font-bold text-xl'>Extended codename: {extended_codename}</h2>
                         </div>
-                        <div className='w-full'>
+                        <div className='h-2 md:h-4'></div>
+                        <div className='w-full px-4'>
                             <h1 className='font-bold text-2xl'>Builds</h1>
+                            <div className='h-1 md:h-2'></div>
                             <Accordion className='w-full'>
-                                {builds.map((build: any) => (
-                                    <AccordionTab>{build["version"]}</AccordionTab>
-                                ))}
+                                {builds.map((build: any) => {
+                                    if (build["direct_download"]) {
+                                        return <AccordionTab 
+                                            header={<div className='flex justify-between w-full'>
+                                                <div>
+                                                    <p>{build["version_name"]}</p>
+                                                </div>
+                                                <div>
+                                                    <p>{build["build_date"]}</p>
+                                                </div>
+                                            </div>}>
+                                                <p>Android {build["android_version"]}</p>
+                                                <p>Kernel: Linux {build["kernel_version"]}</p>
+                                                <p>Direct</p>
+                                        </AccordionTab>
+                                    }
+                                    return <AccordionTab 
+                                        header={<div className='flex justify-between w-full'>
+                                            <div>
+                                                <p>{build["version_name"]}</p>
+                                            </div>
+                                            <div>
+                                                <p>{build["build_date"]}</p>
+                                            </div>
+                                        </div>}>
+                                            <p>Android {build["android_version"]}</p>
+                                            <p>Kernel: Linux {build["kernel_version"]}</p>
+                                    </AccordionTab>
+                                })}
                             </Accordion>
                         </div>
                     </div>
