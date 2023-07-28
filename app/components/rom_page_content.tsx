8fe35b4e32d3b9ef66f5ece17483401575f9346c
PiotrZPL <zpl20052020@protonmail.ch>
2023-07-28 20:11:36 +0200

    Added more styles

--- app/components/rom_page_content.tsx
+++ app/components/rom_page_content.tsx
@@ -18,7 +18,7 @@ export default function RomPageContent( props: any ) {
                 <div className='md:w-2/6 bg-gray-800 rounded-lg border-2 md:border-4 border-white'>
                     <div className='flex flex-col'>
                         {/* <div className='w-1/12 md:w-0'></div> */}
-                        <div className='px-4'>
+                        <div className='px-4 pt-2'>
                             <h1 className='font-bold text-4xl'>{props["romName"]}</h1>
                             <div className='h-2 md:h-4'></div>
                             <h2 className='font-bold text-xl py-1'>Supported on {romDevices.length} device</h2>
@@ -47,9 +47,9 @@ export default function RomPageContent( props: any ) {
                                                         <p>{build["build_date"]}</p>
                                                     </div>
                                                 </div>}>
-                                                    <p>Android {build["android_version"]}</p>
-                                                    <p>Kernel: Linux {build["kernel_version"]}</p>
-                                                    <div className='flex flex-col items-center'>
+                                                    <p className='font-bold'>Android {build["android_version"]}</p>
+                                                    <div className='flex'><p>Kernel:&nbsp;</p><p className='font-bold'>Linux {build["kernel_version"]}</p></div>
+                                                    <div className='flex flex-col items-center pt-2'>
                                                         <button className='bg-android rounded-md w-fit'>
                                                             <a target="_blank" rel="noopener noreferrer" href={`https://cdn.piotrlange.de/${build["file_name"]}`}>
                                                                 <p className='p-2 text-lg md:text-2xl font-bold'><i className='jtbIL-arrow-down-to-bracket'></i> Direct download</p>
@@ -74,9 +74,9 @@ export default function RomPageContent( props: any ) {
                                                     <p>{build["build_date"]}</p>
                                                 </div>
                                             </div>}>
-                                                <p>Android {build["android_version"]}</p>
-                                                <p>Kernel: Linux {build["kernel_version"]}</p>
-                                                <div className='flex flex-col items-center'>
+                                                <p className='font-bold'>Android {build["android_version"]}</p>
+                                                <div className='flex'><p>Kernel:&nbsp;</p><p className='font-bold'>Linux {build["kernel_version"]}</p></div>
+                                                <div className='flex flex-col items-center pt-2'>
                                                     <button className='bg-sf rounded-md w-fit'>
                                                         <a target="_blank" rel="noopener noreferrer" href={build["sourceforge_link"]}>
                                                             <p className='p-2 text-lg md:text-2xl font-bold'><i className='jtbIB-sourceforge'></i> Download from SourceForge</p>
@@ -90,10 +90,9 @@ export default function RomPageContent( props: any ) {
                         </div>
                     </div>
                 </div>
-                <div className='md:w-1/2 flex flex-col items-center'>
+                <div className='md:w-1/2 flex flex-col items-center pt-4 md:pt-0'>
                     <h1 className='font-bold text-3xl'>Devices</h1>
-                    <div className='h-10'></div>
-                    <img src={"https://raw.githubusercontent.com/PiotrZPL/PiotrZPLBuilds/main/images/" + extended_codename + ".webp"} alt={extended_codename} height={500}></img>
+                    <img className='pt-4 md:pt-8' src={"https://raw.githubusercontent.com/PiotrZPL/PiotrZPLBuilds/main/images/" + extended_codename + ".webp"} alt={extended_codename} height={500}></img>
                     <div className='h-4'></div>
                     <h1 className='font-bold text-xl'>{extended_codename}</h1>
                 </div>
